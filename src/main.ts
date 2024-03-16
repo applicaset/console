f1a7b7d4335987eec0edc8640574c7ee58c09a6f
Naser Mirzaei <nasermirzaei89@gmail.com>
2024-03-16 19:39:00 -0400

    Remove style.css

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import { createApp } from "vue";
 import { useDataStore } from "@/store/data";
 import "@fontsource/open-sans";
-import "@/style.css";
 
 const app = createApp(App);
 
